06c5c473bcc69c7c943744d953404e354d2de44e
Jochen Schmähling <js1157@gmail.com>
2022-10-18 20:13:57 +0200

    Version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyvcontrol',
-      version='1.4.4',
+      version='1.4.5',
       description='Communication with Viessmann heating via Optolink interface.',
       url='http://github.com/',
       author='Jochen Schmaehling',
