dae2beb5543e8cd92082a22a0d2a21343fbc5f55
Jochen Schmähling <js1157@gmail.com>
2026-01-03 15:44:36 +0100

    private method

--- src/pyvcontrol/vi_control.py
+++ src/pyvcontrol/vi_control.py
@@ -66,21 +66,21 @@ class ViControl:
     def execute_read_command(self, command_name) -> ViData:
         """Sends a read command and gets the response."""
         vc = ViCommand(command_name)
-        return self.execute_command(vc, "read")
+        return self._execute_command(vc, "read")
 
     def execute_write_command(self, command_name, value) -> ViData:
         """Sends a write command and gets the response."""
         vc = ViCommand(command_name)
         vd = ViData.create(vc.unit, value)
-        return self.execute_command(vc, "write", payload=vd)
+        return self._execute_command(vc, "write", payload=vd)
 
     def execute_function_call(self, command_name, *function_args) -> ViData:
         """Sends a function call command and gets response."""
         payload = bytearray((len(function_args), *function_args))
         vc = ViCommand(command_name)
-        return self.execute_command(vc, "call", payload=payload)
+        return self._execute_command(vc, "call", payload=payload)
 
-    def execute_command(self, vc, access_mode, payload=bytes(0)) -> ViData:
+    def _execute_command(self, vc, access_mode, payload=bytes(0)) -> ViData:
         # prepare command
         allowed_access_mode = {"read": ["read"], "write": ["read", "write"], "call": ["call"]}
         if access_mode not in allowed_access_mode[vc.access_mode]:
